70903cfd718b7726078cfa9234e6f99275776099
Saw-jan <saw.jan.grg3e@gmail.com>
2023-02-02 09:41:14 +0545

    with fix

--- .drone/download.sh
+++ .drone/download.sh
@@ -22,8 +22,9 @@ else
   echo "Reuse existing $FROM"
 fi
 
-wget -qO /drone/src/owncloud-705290a-all.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-705290a-all.tar.bz2
-wget -qO /drone/src/owncloud-214dd63-all.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-214dd63-all.tar.bz2
+wget -qO /drone/src/owncloud-master.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-master.tar.bz2
+# wget -qO /drone/src/owncloud-705290a-all.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-705290a-all.tar.bz2
+wget -qO /drone/src/owncloud-214dd63.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-214dd63.tar.bz2
 # wget -qO /drone/src/owncloud-705290a.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-705290a.tar.bz2
 # wget -qO /drone/src/owncloud-214dd63.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-214dd63.tar.bz2
 # wget -qO /drone/src/owncloud-997256c.tar.bz2 https://jankaritech.ocloud.de/index.php/s/akkL82ne9CYK6X2/download\?path=/\&files=owncloud-997256c.tar.bz2
