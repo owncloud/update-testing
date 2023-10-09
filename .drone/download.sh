0a5c112981f959d546becfa9b48453bfb7d5e117
Phil Davis <phil@jankaritech.com>
2023-10-09 11:21:40 +0545

    Upgrade 9.0 to 9.1 to 10

--- .drone/download.sh
+++ .drone/download.sh
@@ -6,7 +6,6 @@ TO_VERSION=$2
 FROM=owncloud-$FROM_VERSION.tar.bz2
 TO=owncloud-$TO_VERSION.tar.bz2
 
-
 if [ ! -f $FROM ]; then
   # Look in download.owncloud.com/server for the tarball
   # All official tarballs will be found there
@@ -33,6 +32,28 @@ if [ ! -f $TO ]; then
   fi
 else
   echo "Reuse existing $TO"
+fi
+
+if [ ! -z "$3" ]; then
+  VIA_VERSION=$3
+  VIA=owncloud-$VIA_VERSION.tar.bz2
+  if [ ! -f $VIA ]; then
+    # Look in download.owncloud.com/server for the tarball
+    # All official tarballs will be found there
+    wget -nv http://download.owncloud.com/server/daily/$VIA || true
+    if [ ! -f $VIA ]; then
+      wget -nv http://download.owncloud.com/server/testing/$VIA || true
+    fi
+    if [ ! -f $VIA ]; then
+      wget -nv http://download.owncloud.com/server/stable/$VIA || true
+    fi
+  else
+    echo "Reuse existing $VIA"
+  fi
+  if [ ! -f $VIA ]; then
+    echo "Could not download $VIA"
+    exit 1
+  fi
 fi
 
 if [ ! -f $FROM ]; then
